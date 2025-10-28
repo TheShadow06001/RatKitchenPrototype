a71d9653d358d32a6f4eeb0782db6fa948c25c9b
Sina Sotudenia <sina.sotudenia@edu.futuregames.se>
2025-10-28 18:48:10 +0100

    Separate player lane change and movement logic into separate scripts

---
+++ RatKitchenProto/Assets/Scripts/Player/PlayerChangeLane.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+
+public class PlayerChangeLane : MonoBehaviour
+{
+    private int lane = 1;
+
+    float leftLaneX = -0.5f;
+    float middleLaneX = 0f;
+    float rightLaneX = 0.5f;
+    private float targetHorizontalX;
+    [SerializeField] private float laneChangeSpeed = 10f;
+
+    void Start()
+    {
+        targetHorizontalX = middleLaneX;
+    }
+
+    void Update()
+    {
+        PlayerChangeLine();
+
+        Vector3 currentPos = transform.position;
+
+        float newX = Mathf.Lerp(currentPos.x, targetHorizontalX, Time.deltaTime * laneChangeSpeed);
+        float newZ = currentPos.z;
+        transform.position = new Vector3(newX, currentPos.y, newZ);
+    }
+    void PlayerChangeLine()
+    {
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            if (lane < 2)
+            {
+                lane++;
+            }
+
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            if (lane > 0)
+            {
+                lane--;
+            }
+        }
+        if (lane == 0)
+        {
+            targetHorizontalX = leftLaneX;
+        }
+        else if (lane == 1)
+        {
+            targetHorizontalX = middleLaneX;
+        }
+        else if (lane == 2)
+        {
+            targetHorizontalX = rightLaneX;
+        }
+    }
+}
--- RatKitchenProto/Assets/Scripts/Player/PlayerMovement.cs
+++ RatKitchenProto/Assets/Scripts/Player/PlayerMovement.cs
@@ -1,103 +1,16 @@
-using JetBrains.Annotations;
-using UnityEditor.Experimental.GraphView;
-using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
-using static UnityEngine.Rendering.DebugUI;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] GameObject Camera;
-    [SerializeField] GameObject anchor;
-    private float moveSpeed;
-    private float baseSpeed;
-
-    private int lane = 1;
-
-    float leftLaneX = -0.5f;
-    float middleLaneX = 0f;
-    float rightLaneX = 0.5f;
-
-    Vector3 moveDirection;
-    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        baseSpeed = Camera.GetComponent<CameraScript>().moveSpeed;
-
-        moveSpeed = baseSpeed;
-
-    }
-
-    void Update()
-    {
-        PlayerMoveForward();
-        PlayerChangeLine();
-    }
-    void PlayerMoveForward()
-    {
-        if (gameObject.transform.position.z < anchor.transform.position.z)
-        {
-            if (moveSpeed < baseSpeed * 2)
-            {
-                moveSpeed *= 2;
-            }
-        }
-        else
-        {
-            moveSpeed = baseSpeed;
-        }
-
-        if (Input.GetKey(KeyCode.Space))
-        {
-            return;
-        }
-        else 
-        {
-            transform.position += transform.forward * moveSpeed * Time.deltaTime;
-        }
-
-            
-    }
-    void PlayerChangeLine()
-    {
-        if (Input.GetKeyDown(KeyCode.D))
-        {
-            if (lane < 2)
-            {
-                lane++;
-                SwapLane(lane);
-            }
-
-        }
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            if (lane > 0)
-            {
-                lane--;
-                SwapLane(lane);
-            }
-
-        }
         
     }
 
-    private void SwapLane(int value)
+    // Update is called once per frame
+    void Update()
     {
-        Vector3 currentPos = transform.position;
-        if (value == 1)
-        {
-            currentPos.x = middleLaneX;
-        }
-        else if (value == 2)
-        {
-            currentPos.x = rightLaneX;
-        }
-        else
-        {
-            currentPos.x = leftLaneX;
-        }
-        transform.position = currentPos;
+        
     }
-
-
 }
-
