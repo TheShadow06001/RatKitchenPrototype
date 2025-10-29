0301c3e49486135bfc0947dbdbc217cd0bc37719
Sina Sotudenia <sina.sotudenia@edu.futuregames.se>
2025-10-29 18:00:46 +0100

    Update player movement script and scene

--- RatKitchenProto/Assets/Scripts/Player/PlayerMovement.cs
+++ RatKitchenProto/Assets/Scripts/Player/PlayerMovement.cs
@@ -6,13 +6,20 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] GameObject anchorPoint;
     float moveSpeed;
     float cameraSpeed;
+    [SerializeField] float forwardAcceleration = 1f;
+    [SerializeField] float maxSpeedMultiplier = 2f;
     [SerializeField] float jumpForce = 5f;
+    [SerializeField] float dashForce = 20f;
     [SerializeField] PlayerChangeLane laneChanger;
+
+    private Rigidbody rb;
+
     void Start()
     {
         cameraSpeed = mainCamera.GetComponent<CameraScript>().moveSpeed;
         moveSpeed = cameraSpeed;
 
+        rb = GetComponent<Rigidbody>();
         laneChanger = laneChanger.GetComponent<PlayerChangeLane>();
     }
 
@@ -20,31 +27,36 @@ public class PlayerMovement : MonoBehaviour
     {
         HandleForwardSpeed();
         Jump();
+        HandleDash();
         Vector3 currentPos = transform.position;
 
-        if (Input.GetKey(KeyCode.Space) == false)
+        if (!Input.GetKey(KeyCode.Space))
         {
             currentPos.z += moveSpeed * Time.deltaTime;
+            transform.position = new Vector3(currentPos.x, currentPos.y, currentPos.z);
         }
-        transform.position = new Vector3(currentPos.x, currentPos.y, currentPos.z);
     }
     void HandleForwardSpeed()
     {
-        if (gameObject.transform.position.z < anchorPoint.transform.position.z)
+        if (transform.position.z < anchorPoint.transform.position.z)
         {
-            if (moveSpeed < cameraSpeed * 2)
-            {
-                moveSpeed *= 2f;
-            }
+            float targetSpeed = cameraSpeed * maxSpeedMultiplier;
+            moveSpeed = Mathf.Lerp(moveSpeed, targetSpeed, Time.deltaTime * forwardAcceleration);
         }
         else
         {
             moveSpeed = cameraSpeed;
         }
     }
+    void HandleDash()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift) && !laneChanger.isChangingLanes)
+        {
+            rb.AddForce(Vector3.forward * dashForce, ForceMode.Impulse);
+        }
+    }
     void Jump()
     {
-        Rigidbody rb = GetComponent<Rigidbody>();
         if (rb != null && Input.GetKeyDown(KeyCode.F) && transform.position.y < 1.05 && !laneChanger.isChangingLanes)
         {
                 rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
