0f498635fb120679967fdbbff01518dad2a0b3e8
Marreius2005 <Marius.Sommer@edu.futuregames.se>
2025-10-30 13:38:37 +0100

    MEow

--- RatKitchenProto/Assets/Scripts/Knife.cs
+++ RatKitchenProto/Assets/Scripts/Knife.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 public class Knife : MonoBehaviour
 {
     [SerializeField] private float speed = 1;
+    [SerializeField] private float minRate = 0;
+    [SerializeField] private float maxRate = 2;
+
+    [SerializeField] private MeshRenderer mesh;
+    [SerializeField] private MeshRenderer line;
 
     private float endRotation;
 
@@ -12,19 +17,19 @@ public class Knife : MonoBehaviour
 
     void Start()
     {
-        
+
         endRotation = 88f;
 
         isComplete = false;
         StartCoroutine(Chop());
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-        if (isComplete) 
+
+        if (isComplete)
         {
             isComplete = false;
             StartCoroutine(Chop());
@@ -33,27 +38,47 @@ public class Knife : MonoBehaviour
 
     IEnumerator Chop()
     {
+        line.enabled = true;
+        mesh.enabled = true;
+
+        yield return new WaitForSeconds(0.3f);
+
         for (float i = 0; i < endRotation; i += Time.deltaTime * speed)
         {
-            //transform.rotation = Quaternion.Slerp(startRotation, endRotation, i);
             transform.rotation = Quaternion.Euler(i, 0, 0);
             yield return null;
+
+            SoundManager.Instance.PlaySoundEffect(SoundManager.SoundEffects.RatJumping);
         }
 
+
+
         yield return new WaitForSeconds(0.1f);
-        for (float i = 0; i < endRotation; i += Time.deltaTime * speed/15)
+
+        line.enabled = false;
+        mesh.enabled = false;
+
+        for (float i = 0; i < endRotation; i += Time.deltaTime * speed / 15)
         {
             transform.rotation = Quaternion.Euler(endRotation - i, 0, 0);
             yield return null;
         }
-        
+
+
+
+
+        float value = Random.Range(minRate, maxRate);
+
+        yield return new WaitForSeconds(value);
         isComplete = true;
     }
 
-   
+
+
+
 
     private void OnCollisionEnter(Collision collision)
     {
-        
+
     }
 }
