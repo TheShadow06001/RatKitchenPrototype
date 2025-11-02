62fdc060673c52933035a230b200443ad265669a
@Spexuz <FilipChristianPerneklo@Gmail.com>
2025-11-02 01:53:33 +0100

    Good Night

--- RatKitchenProto/Assets/Backend/Scripts/SoundManager.cs
+++ RatKitchenProto/Assets/Backend/Scripts/SoundManager.cs
@@ -31,13 +31,13 @@ public enum SoundEffects
     GasStoveFire,
     BlenderBlending,
     GameplayMusic,
-    ButtonPress,
-    ButtonHover,
     
     // Music
     
+    
     // UI Sounds
-
+    ButtonPress,
+    ButtonHover,
 }
 
 public class SoundManager : MonoBehaviour
