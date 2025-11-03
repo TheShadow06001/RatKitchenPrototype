61006ca3d4535319ca54e866d657fce08416b9f3
SinaPlays <30eana@gmail.com>
2025-11-03 14:14:25 +0100

    Update camera and player scripts, modify Map scene, and add prefabs

--- RatKitchenProto/Assets/Backend/Scripts/Player/PlayerMovement.cs
+++ RatKitchenProto/Assets/Backend/Scripts/Player/PlayerMovement.cs
@@ -1,25 +1,27 @@
+using System.Collections;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] GameObject mainCamera;
     [SerializeField] GameObject anchorPoint;
-    [SerializeField] float forwardAcceleration = 1f;
-    [SerializeField] float backAcceleration = 3f;
-    [SerializeField] float minSpeedMultiplier = 0.5f;
-    [SerializeField] float maxSpeedMultiplier = 2f;
+    [SerializeField] float forwardAcceleration = 1.05f;
+    [SerializeField] float maxSpeedMultiplier = 1.05f;
     [SerializeField] float jumpForce = 5f;
     [SerializeField] PlayerChangeLane laneChanger;
 
-    [SerializeField] float dashSpeed = 10f;
-    [SerializeField] float dashFadeTime = 0.5f;
-    private float currentDashSpeed = 0f;
+    [Header("Keybinds")]
+    public KeyCode _PlayerJump;
 
     float moveSpeed;
     float cameraSpeed;
+    Rigidbody rigidBody;
+
 
     void Start()
     {
+        rigidBody = GetComponent<Rigidbody>();
+
         cameraSpeed = mainCamera.GetComponent<CameraScript>().moveSpeed;
         moveSpeed = cameraSpeed;
 
@@ -28,52 +30,38 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
+
         HandleForwardSpeed();
         Jump();
-        HandleDash();
         Vector3 currentPos = transform.position;
-        if (currentDashSpeed > 0.1)
-        {
-            currentDashSpeed = Mathf.Lerp(currentDashSpeed, 0.01f, Time.deltaTime / dashFadeTime);
-        }
-        if (!Input.GetKey(KeyCode.Space))
-        {
-            float totalSpeed = moveSpeed + currentDashSpeed;
 
-            currentPos.z += totalSpeed * Time.deltaTime;
-            transform.position = new Vector3(currentPos.x, currentPos.y, currentPos.z);
-        }
+        float totalSpeed = moveSpeed;
+
+        currentPos.x -= totalSpeed * Time.deltaTime;
+        transform.position = new Vector3(currentPos.x, currentPos.y, currentPos.z);
+
+
+
     }
     void HandleForwardSpeed()
     {
-        if (transform.position.z < anchorPoint.transform.position.z - 0.1f)
+        if (transform.position.x > anchorPoint.transform.position.x + 0.1f)
         {
             float targetSpeed1 = cameraSpeed * maxSpeedMultiplier;
             moveSpeed = Mathf.MoveTowards(moveSpeed, targetSpeed1, Time.deltaTime * forwardAcceleration);
-        }
-        else if (transform.position.z > anchorPoint.transform.position.z + 0.1f)
-        {
-            float targetSpeed2 = cameraSpeed * minSpeedMultiplier;
-            moveSpeed = Mathf.MoveTowards(moveSpeed, targetSpeed2, Time.deltaTime * backAcceleration);
         }
         else
         {
             moveSpeed = cameraSpeed;
         }
     }
-    void HandleDash()
-    {
-        if (Input.GetKeyDown(KeyCode.LeftShift) && !laneChanger.isChangingLanes && currentDashSpeed <= 0.1f)
-        {
-            currentDashSpeed = dashSpeed;
-        }
-    }
+
     void Jump()
     {
-        Rigidbody rb = GetComponent<Rigidbody>();
-        if (rb != null && Input.GetKeyDown(KeyCode.F) && transform.position.y < 1.05 && !laneChanger.isChangingLanes)
+
+        if (rigidBody != null && Input.GetKeyDown(_PlayerJump) && transform.position.y < 1.1 && !laneChanger.isChangingLanes)
         {
-                rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            rigidBody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
         }
     }
 }
