d052279b55f9b6324484bd618e2c0a6b2997e03e
Clovexx <bossestrom2@gmail.com>
2025-11-04 12:10:17 +0100

    StateMachine Preset

---
+++ RatKitchenProto/Assets/Backend/Scripts/StateMachine/State.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class State : MonoBehaviour
+{
+    public virtual void EnterState()
+    {
+
+    }
+
+    public virtual void ExitState()
+    {
+
+    }
+
+    public virtual void UpdateState()
+    {
+
+    }
+}
